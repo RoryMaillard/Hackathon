0006c54ed7ebde3c6c843ca13164e7e17e35ae61
root <root@DESKTOP-S8SDAPI>
2024-01-09 16:48:10 +0100

    v2

--- front_src/App.vue
+++ front_src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import homepage from './vues/homepage.vue'
 </script>
 
 <template>
@@ -8,7 +9,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <homepage/>
     </div>
   </header>
 
