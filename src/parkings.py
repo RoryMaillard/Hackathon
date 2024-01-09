8e9f09d2110231b3488f56164761f5d89bff8a56
Samcahuete <114082524+Samcahuete@users.noreply.github.com>
2024-01-09 11:39:34 +0100

    Api flask (#5)
    * premier point d'entrée pour la récupération des parkings + clef api
    
    * mini front
    
    ---------
    
    Co-authored-by: RoryMaillard <98741092+RoryMaillard@users.noreply.github.com>

--- src/parkings.py
+++ src/parkings.py
@@ -1,8 +1,8 @@
-import json
 import os
+import flask
 import requests
-from flask import Flask, jsonify, make_response
 
+from flask import Flask, make_response
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,12 +14,15 @@ PORT = 3202
 HOST = 'localhost'
 
 
+@app.route("/", methods=['GET'])
+def get_homepage():
+    return flask.render_template('index.html.jinja2')
+
 @app.route("/parkings", methods=['GET'])
 def get_parkings():
     parkings = requests.get(f"https://data.nantesmetropole.fr/api/explore/v2.1/catalog/datasets/244400404_parkings-publics-nantes-disponibilites/records?apikey={os.getenv('API_KEY')}")
     res = make_response(parkings.json(), 200)
     return res
-
 
 if __name__ == "__main__":
     print("Server running in port %s" % PORT)
