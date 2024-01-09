942d723c09a0ca6b257dbf76f3417cec46c893b9
root <root@DESKTOP-S8SDAPI>
2024-01-09 22:34:57 +0100

    Premier essai du front,il  manque la connexion avec le back pour la récupération de donnée et le design dégueulasse ça pique les yeux

--- front_src/main.js
+++ front_src/main.js
@@ -1,7 +1,8 @@
-import './assets/main.css'
+import './assets/main.css';
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.css';
 
-createApp(App).mount('#app')
+
+createApp(App).mount('#app');
