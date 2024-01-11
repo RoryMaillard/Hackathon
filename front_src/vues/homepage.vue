f2f49a4bda859e024ebda68084ce5dd69f5a4327
root <root@DESKTOP-S8SDAPI>
2024-01-11 09:38:23 +0100

    test du front

--- front_src/vues/homepage.vue
+++ front_src/vues/homepage.vue
@@ -34,15 +34,16 @@ const updateFilteredEvents = (updatedSelectedCategories) => {
 async function getFilteredEvents(categories){
   const configHTTP = {
     method:"GET",
-    url:"http://127.0.0.1:5000/activites",
+    url:"http://localhost:5001/activites",
     headers:{
       'Content-Type': 'application/json'
-    }
+    },
+    withCredentials:true,
   }
   try{
-    const events = (await axios.request(configHTTP));
+    const events = (await axios.request(configHTTP)).data;
     console.log("events", events)
-    return [{"accueil_enfant":"non","adresse":"1 Rue Eugène Thomas","annule":"non","categorie_1":"Activité à partager","categorie_2":null,"categorie_3":null,"categorie_4":null,"categorie_5":null,"code_postal":"44300","complet":"non","courriel":null,"date":"2024-02-08","date_report":null,"description":"Un atelier en petit groupe pour échanger, partager, pratiquer et améliorer votre français en toute convivialité.","emetteur":"BIBLIOTHEQUE DE NANTES","gratuit":"oui","heure_debut":"14:30","heure_fin":null,"id_manif":"40494","info_suppl":null,"libelle_courriel":null,"libelle_site":null,"libelle_telephone":null,"lien_agenda":"https://metropole.nantes.fr/infonantes/agenda/40494","lieu":"Médiathèque Luce Courville","lieu_quartier":"Nantes Nord","lieu_siteweb":"http://bm.nantes.fr","lieu_tel":"02 40 41 53 50","media_url":"https://metropole.nantes.fr//banque/public/images/culture/c/40494-1-conversation-en-francais.jpg","nom":"Conversation en français","organisateurs":null,"precisions_public":"Tout public","precisions_tarifs":null,"reporte":"non","rubrique":"Culture","telephone":null,"url_site":null,"ville":"Nantes"}]
+    return events;
   }catch(error){
     return [];
     alert(error);
