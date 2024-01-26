0a1d30e59f303a6e2ffa8a894286ad625ee8bbbe
Arnaud-Parion <arnaud.parion@gmail.com>
2024-01-26 12:12:42 +0100

    ajout des categories gratuit, malentendant, malvoyant et deficient intellectuel

--- front/front_src/components/categorySelection.vue
+++ front/front_src/components/categorySelection.vue
@@ -1,44 +1,85 @@
-<!-- src/components/categorySelection.vue -->
 <template>
   <div class="width-100">
-    <h2>Catégories</h2>
-    <button
+    <h2>Choose Categories</h2>
+    <div>
+      <button
         v-for="category in categories"
         :key="category"
-        class="btn mb-1 inline-text"
+        class="btn mb-1 mr-05 inline-text"
         :class="{ 'btn-secondary': !selectedCategories.includes(category), 'btn-primary': selectedCategories.includes(category) }"
         @click="toggleCategory(category)"
-    >
-      {{ category }}
-    </button>
-    <button class="btn btn-success" @click="$emit('updateFilteredEvents', selectedCategories)">Rechercher</button>
+      >
+        {{ category }}
+      </button>
+    </div>
+    <div class="d-flex mt-4">
+      <button
+        class="btn mb-1 mr-05 inline-text"
+        :class="{ 'btn-secondary': !selectedCategories.includes('Gratuit'), 'btn-primary': selectedCategories.includes('Gratuit') }"
+        @click="toggleCategory('Gratuit')"
+      >
+        Gratuit
+      </button>
+      <button
+        class="btn mb-1 mr-05 inline-text"
+        :class="{ 'btn-secondary': !selectedCategories.includes('h_auditif'), 'btn-primary': selectedCategories.includes('h_auditif') }"
+        @click="toggleCategory('h_auditif')"
+      >
+        Malentendant
+      </button>
+      <button
+        class="btn mb-1 mr-05 inline-text"
+        :class="{ 'btn-secondary': !selectedCategories.includes('h_intellectuel'), 'btn-primary': selectedCategories.includes('h_intellectuel') }"
+        @click="toggleCategory('h_intellectuel')"
+      >
+        Déficient intellectuel
+      </button>
+      <button
+        class="btn mb-1 mr-05 inline-text"
+        :class="{ 'btn-secondary': !selectedCategories.includes('h_visuel'), 'btn-primary': selectedCategories.includes('h_visuel') }"
+        @click="toggleCategory('h_visuel')"
+      >
+        Malvoyant
+      </button>
+      <button class="btn btn-success" @click="updateFilteredEvents">Rechercher</button>
+    </div>
   </div>
 </template>
 
-<script setup>
-import { ref } from 'vue';
-
-const emits = defineEmits(['updateFilteredEvents'])
-const props = defineProps({
-  categories: Array,
-  selectedCategories: Array,
-});
-
-const selectedCategories = ref(props.selectedCategories);
-
-const toggleCategory = (categoryName) => {
-  const index = selectedCategories.value.indexOf(categoryName);
-  index === -1 ? selectedCategories.value.push(categoryName) : selectedCategories.value.splice(index, 1);
+<script>
+export default {
+  props: {
+    categories: Array,
+    selectedCategories: Array,
+  },
+  computed: {
+    selectedCategories: function() {
+      return [...this.selectedCategories];
+    }
+  },
+  methods: {
+    toggleCategory(categoryName) {
+      const index = this.selectedCategories.indexOf(categoryName);
+      index === -1
+        ? this.selectedCategories.push(categoryName)
+        : this.selectedCategories.splice(index, 1);
+    },
+    updateFilteredEvents() {
+      this.$emit('updateFilteredEvents', this.selectedCategories);
+    },
+  },
 };
-
 </script>
 
 <style scoped>
-.mr-1 {
-  margin-right: 1rem;
+.mr-05 {
+  margin-right: 0.5rem;
 }
 .mb-1 {
   margin-bottom: 1rem;
+}
+.mt-4 {
+  margin-top: 4rem;
 }
 .inline-text {
   white-space: nowrap;
@@ -54,6 +95,6 @@ const toggleCategory = (categoryName) => {
 }
 .btn-primary {
   background-color: #007bff; /* Change this to your preferred color */
-  color: #ffffff; /* Change this to your preferred text color */
+  color: #000000; /* Change this to your preferred text color */
 }
 </style>
